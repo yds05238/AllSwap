a3c6185fdb6d30c3f8f3f6b16c1b4cf8dbe138e4
Youn Sung Huh <yh496@cornell.edu>
2020-01-27 21:58:44 -0500

    images loading

--- Allswap_django/allswap/allswap/settings.py
+++ Allswap_django/allswap/allswap/settings.py
@@ -15,6 +15,7 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATE_DIR = os.path.join(BASE_DIR,"templates")
+STATIC_DIR = os.path.join(BASE_DIR, "static")
 
 MEDIA_ROOT = os.path.join(BASE_DIR,'media/')
 MEDIA_URL = '/media/'
@@ -127,7 +128,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIR = [os.path.join(BASE_DIR,'static')]
+STATICFILES_DIRS = [
+    STATIC_DIR,
+]   
+    
 
 LOGIN_REDIRECT_URL = 'test'
 LOGOUT_REDIRECT_URL = 'thanks'
--- Allswap_django/allswap/allswap/urls.py
+++ Allswap_django/allswap/allswap/urls.py
@@ -27,6 +27,4 @@ urlpatterns = [
     path('accounts/',include("accounts.urls",namespace = "accounts")),
     path('accounts/',include("django.contrib.auth.urls")),
     path('products/',include("products.urls",namespace = "products")),
-] 
-urlpatterns += staticfiles_urlpatterns()
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
