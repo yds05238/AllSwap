15e70926ccb467649728785afd10d8ed1cece6bf
Youn Sung Huh <yh496@cornell.edu>
2020-02-15 09:22:57 -0500

    user login/signup added

--- Allswap_djangoREST/backend/allswap/allswap/settings.py
+++ Allswap_djangoREST/backend/allswap/allswap/settings.py
@@ -37,11 +37,20 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
+    'rest_auth.registration',
     'rest_framework',
+    'rest_framework.authtoken',
+    'rest_auth',
+
     'products',
     'corsheaders',
 ]
 
+SITE_ID = 1
 
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
@@ -134,3 +143,9 @@ REST_FRAMEWORK = {
 }
 
 CORS_ORIGIN_ALLOW_ALL = True
+
+ACCOUNT_EMAIL_VERIFICATION = 'none'
+
+ACCOUNT_AUTHENTICATION_METHOD = 'username'
+
+ACCOUNT_EMAIL_REQUIRED = False
--- Allswap_djangoREST/backend/allswap/allswap/urls.py
+++ Allswap_djangoREST/backend/allswap/allswap/urls.py
@@ -18,6 +18,9 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('rest-auth/', include('rest_auth.urls')),
+    path('rest-auth/registration/', include('rest_auth.registration.urls')),
     path('api-auth/', include('rest_framework.urls')),
     path('api/', include('products.api.urls')),
+
 ]
